cb98f2a236fdb08cfaa6846285c0cc70d9427df1
onpreeya <83701404+onpreeya-srisai@users.noreply.github.com>
2022-09-19 23:47:50 +0700

    connect database

--- index.php
+++ index.php
@@ -41,16 +41,26 @@
 <body>
 <?php
     session_start();
-    $servername = "localhost";
-    $username = "root";
-    $password = "12345678";
-    $dbname = "shop";
+//     $servername = "localhost";
+//     $username = "root";
+//     $password = "12345678";
+//     $dbname = "shop";
     $per_page=10;
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
     if(isset($_GET["page"])) $start_page=$_GET["page"]*$per_page;
     else $start_page=0;
     
-    $con=mysqli_connect($servername,$username,$password,$dbname);
+//     $con=mysqli_connect($servername,$username,$password,$dbname);
     if(!$con) die("Connnect mysql database fail!!".mysqli_connect_error());
     // echo "Connect mysql successfully!";
     echo "<h1>Welcome to CANOTWAIT_</h1>";
